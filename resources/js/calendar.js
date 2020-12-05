b7bbdd6c2cffe051e7b7fdbcf0ba76f399bfaac6
Andrew O'Kins <aokins22@whitworth.edu>
2020-12-05 12:44:47 -0800

    Calendar loads title of tasks on dates > Need to consider character lengths and number of tasks on date > Added more tasks on a date to see lot of tasks case

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -11,8 +11,25 @@ var monthTag = document.getElementById("month")
 var yearTag = document.getElementById("year")
 var datesObj = document.getElementsByClassName("calendarDates")[0];
 
-function generateCalendar(month, year) {
-    // First empty the Nodes of children if they are not empty
+// Function to append list task items into a calendar date
+// Input: node for where the current date item is
+//        date for what the date is to access the correct tasks
+function loadTasksForDate(node, date, jsonData) {
+    var dateS = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + (date.getDate());
+    var listItem;
+    for (task in jsonData[dateS] ) {
+        listItem = document.createElement("LI");
+        listItem.appendChild(document.createTextNode(jsonData[dateS][task].title));
+        node.appendChild(listItem);
+    }
+    
+}
+
+
+
+async function generateCalendar(month, year, jsonContent) {
+    // Fetch the data and wait for response
+        // First empty the Nodes of children if they are not empty
     monthTag.innerHTML = '';
     yearTag.innerHTML = '';
     datesObj.innerHTML = '';
@@ -26,8 +43,8 @@ function generateCalendar(month, year) {
 
     // Offset backwards to start of week (for example, Monday would have 1 day before which is Sunday)
     date.setDate(date.getDate() - date.getDay());
-
     // First iterate through the spaces that would be the previous month, depends on day of the week that current month starts at
+
     for (; date.getMonth() != month; date.setDate(date.getDate()+1)) {
         var dNode = document.createElement("UL");
         dNode.setAttribute("class", "date past"); // Previous month dates are given the class called past
@@ -39,6 +56,7 @@ function generateCalendar(month, year) {
         datesObj.appendChild(dNode);
 
         // Append tasks for this date here
+        loadTasksForDate(dList, date, jsonContent);
     }
     // Now iterate through each date through the month, CSS handles the individual weeks
     for (; date.getMonth() == month; date.setDate(date.getDate()+1)) {
@@ -54,6 +72,7 @@ function generateCalendar(month, year) {
         datesObj.appendChild(dNode);
     
         // Append tasks for this date here
+        loadTasksForDate(dList, date, jsonContent);
     }
 
     // Now iterate through the spaces that would be the next month for the rest of the week if this month ended before Saturday
@@ -67,23 +86,31 @@ function generateCalendar(month, year) {
         datesObj.appendChild(dNode);
 
         // Append tasks for this date here
+        loadTasksForDate(dList, date, jsonContent);
     }    
 }
 
 
 // Script function called when the html page has loaded, handles loading the page content
 async function loadContent()  {
-    // Default loading is to current month and year
-    generateCalendar(now.getMonth(), now.getFullYear());
+    // Default loading is to current month and year    // Fetch the data and wait for response
+    let data = await fetch(location.protocol + "//" + location.host + ":80" + '/test_data/today.json');
+    if (!data.ok) {
+        alert("Could not establish connection to server");
+    }
+    // If received the data okay, call outputTasks with JSON parsing of the content
+    else {
+        data.text().then((content) => generateCalendar(currentMonth.getMonth(), currentMonth.getFullYear(), JSON.parse(content)));
+    }
 }
 
 // Function called by navigation buttons that move forward or backward by one month
-function changeMonth(forward) {
+async function changeMonth(forward) {
     if (forward) {
         currentMonth.setMonth(currentMonth.getMonth() + 1);
     }
     else {
         currentMonth.setMonth(currentMonth.getMonth() - 1);
     }
-    generateCalendar(currentMonth.getMonth(), currentMonth.getFullYear());
+    loadContent();
 }
